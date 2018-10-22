41c8ddc4342e5bec1dc87796e2257e67c261f135
makescv <jeonginseok86@gmail.com>
2018-10-22 17:02:14 +0900

    result in output/temp3

---
+++ src/main/java/WordCount/WordCombined1.java
@@ -0,0 +1,193 @@
+package WordCount;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
+import org.apache.hadoop.mapreduce.lib.jobcontrol.JobControl;
+import java.nio.ByteBuffer;
+
+
+public class WordCombined1 extends Configured implements Tool {
+
+    public static class WordMapper extends Mapper<LongWritable, Text, IntWritable, IntWritable> {
+        IntWritable intWritable = new IntWritable();
+        IntWritable one = new IntWritable(1);
+
+        @Override //아마 키가 줄수지
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            //입력 처리
+            String line = value.toString();
+            String word[]=line.split("\\W+");
+
+            int id,loc;
+            id=Integer.parseInt(word[0]);
+            //String timeimsi[]=word[2].split(":");//분초 분리//simple
+            //length=Integer.parseInt(timeimsi[0])*60+Integer.parseInt(timeimsi[1]);//전부 초단위로
+            //loc=(int)word[3].charAt(0)-(int)'A';//알파벳 A부터 0으로
+
+            //mydata=new CompositeWritable(id, length, loc);//성능고려해야함
+            intWritable.set(id);
+            //text.set(word);
+
+            context.write(intWritable, one);
+        }
+    }
+
+    public static class SumReducer extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+        IntWritable intWritable = new IntWritable();
+
+        @Override
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+            int wordCount = 0;
+            for (IntWritable value : values) {
+                wordCount += value.get();
+            }
+            intWritable.set(wordCount);
+            context.write(key, intWritable);
+        }
+    }
+
+    //벨류로 정렬해준다!
+    //단순 키벨류 교환
+    public static class WordMapper2 extends Mapper< Text, Text, IntWritable, IntWritable> {
+        //IntWritable frequency = new IntWritable();
+
+        @Override
+        public void map(Text key, Text value, Context context)
+                throws IOException, InterruptedException {
+            int k1 = Integer.parseInt(key.toString());
+            int v1 = Integer.parseInt(value.toString());
+            IntWritable k2=new IntWritable(v1);//크로스 해서 정렬시키자
+            IntWritable v2=new IntWritable(k1);
+            context.write(k2, v2);
+        }
+    }
+
+    public static class SumReducer2 extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+        int count=0;
+        @Override
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+            for (IntWritable value : values) {//동점자 스테이블 문제가 발생하겟다 ㅠㅠ 일단 무시
+                if (count++<5)context.write(key,value);
+            }
+        }
+    }
+
+    public static class IntComparator extends WritableComparator {
+
+        public IntComparator() {
+            super(IntWritable.class);
+        }
+
+        @Override
+        public int compare(byte[] b1, int s1, int l1, byte[] b2,
+                           int s2, int l2) {
+            Integer v1 = ByteBuffer.wrap(b1, s1, l1).getInt();
+            Integer v2 = ByteBuffer.wrap(b2, s2, l2).getInt();
+            return v1.compareTo(v2) * (-1);
+        }
+    }
+
+
+
+
+
+
+    public int run(String[] args) throws Exception {
+
+        JobControl jobControl = new JobControl("jobChain");
+        Configuration conf1 = getConf();
+
+        //job1
+        Job job1 = Job.getInstance(conf1);
+        job1.setJarByClass(WordCombined1.class);
+        job1.setJobName("Word Combined");
+
+        FileInputFormat.setInputPaths(job1, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job1, new Path(args[1] + "/temp1"));
+
+        job1.setMapperClass(WordMapper.class);
+        job1.setReducerClass(SumReducer.class);
+        job1.setCombinerClass(SumReducer.class);
+
+        job1.setOutputKeyClass(IntWritable.class);
+        job1.setOutputValueClass(IntWritable.class);
+
+        ControlledJob controlledJob1 = new ControlledJob(conf1);
+        controlledJob1.setJob(job1);
+
+
+        //job2고고! R1정렬해주는거!
+        Configuration conf2 = getConf();
+
+        Job job2 = Job.getInstance(conf2);
+        job2.setJarByClass(WordCombined1.class);
+        job2.setJobName("Word Invert");
+
+        FileInputFormat.setInputPaths(job2, new Path(args[1] + "/temp1"));
+        FileOutputFormat.setOutputPath(job2, new Path(args[1] + "/R1"));
+
+        job2.setMapperClass(WordMapper2.class);
+        job2.setReducerClass(SumReducer2.class);
+        job2.setCombinerClass(SumReducer2.class);
+
+        job2.setOutputKeyClass(IntWritable.class);
+        job2.setOutputValueClass(IntWritable.class);
+        job2.setInputFormatClass(KeyValueTextInputFormat.class);
+
+        job2.setSortComparatorClass(IntComparator.class);
+        ControlledJob controlledJob2 = new ControlledJob(conf2);
+        controlledJob2.setJob(job2);
+
+        // make job2 dependent on job1
+        controlledJob2.addDependingJob(controlledJob1);
+
+        //controlledJob3.addDependingJob(controlledJob1);
+
+        // add the job to the job control
+        jobControl.addJob(controlledJob1);
+        jobControl.addJob(controlledJob2);
+
+        Thread jobControlThread = new Thread(jobControl);
+        jobControlThread.start();
+
+        while (!jobControl.allFinished()) {
+            System.out.println("Jobs in waiting state: " + jobControl.getWaitingJobList().size());
+            System.out.println("Jobs in ready state: " + jobControl.getReadyJobsList().size());
+            System.out.println("Jobs in running state: " + jobControl.getRunningJobList().size());
+            System.out.println("Jobs in success state: " + jobControl.getSuccessfulJobList().size());
+            System.out.println("Jobs in failed state: " + jobControl.getFailedJobList().size());
+            try {
+                Thread.sleep(5000);
+            } catch (Exception e) {
+
+            }
+
+        }
+        System.exit(0);
+        return (job1.waitForCompletion(true) ? 0 : 1);
+    }
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new WordCombined1(), args);
+        System.exit(exitCode);
+    }
+}
+
+
---
+++ src/main/java/WordCount/WordCombined2.java
@@ -0,0 +1,222 @@
+package WordCount;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparator;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
+import org.apache.hadoop.mapreduce.lib.jobcontrol.JobControl;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+
+public class WordCombined2{
+
+
+    public static class IntComparator extends WritableComparator {
+
+        public IntComparator() {
+            super(IntWritable.class);
+        }
+
+        @Override
+        public int compare(byte[] b1, int s1, int l1, byte[] b2,
+                           int s2, int l2) {
+            Integer v1 = ByteBuffer.wrap(b1, s1, l1).getInt();
+            Integer v2 = ByteBuffer.wrap(b2, s2, l2).getInt();
+            return v1.compareTo(v2) * (-1);
+        }
+    }
+
+
+
+    //////R2매퍼들 모음
+
+
+    //R1에서 추린거 읽는거(5명) //이거 거의 비슷하네?
+    //output: 사람, -1
+
+    //원본 읽는거
+    //output: 사람, 지역
+    public static class R2mapper1 extends Mapper<LongWritable, Text, IntWritable, IntWritable> {
+        IntWritable intWritable = new IntWritable();
+        IntWritable intWritable2 = new IntWritable();
+        IntWritable minus = new IntWritable(-1);//R1에서 추린 5명에 해당한다는걸 표현함//독약같은거
+        int top5[]=new int[100];
+
+        @Override
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            //입력 처리
+            String line = value.toString();
+            String word[]=line.split("\\W+");
+            //System.out.println("---------------------------");
+            //System.out.println(key.get());
+            if (word.length>2){//오리지널 인풋 처리
+                int id,loc;
+                id=Integer.parseInt(word[0]);
+                loc=(int)word[3].charAt(0)-(int)'A';//알파벳 A부터 0으로
+                intWritable.set(id);
+                intWritable2.set(loc);
+                context.write(intWritable, intWritable2);//사람 지역
+            }else{ //R1(top5)처리//여기를 reducer의 setup으로 하면 좋을거같은데 어짜피 1개짜리라면
+                int id;
+                id=Integer.parseInt(word[1]);//이게 사람 아이디임(아까 5명 뽑힌거)
+                intWritable.set(id);
+                context.write(intWritable, minus);
+//                top5[id]=1;//기록
+            }
+        }
+    }
+
+    //id -1AABBDDEE <-top5인놈
+    //id AAD <-아닌놈
+    public static class R2SumReducer extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+        IntWritable intWritable = new IntWritable();
+        int locCount[]=new int[100];//100 상수처리, 값을 정하자
+
+        @Override
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int isTop5=0;
+            ArrayList<Integer> arr=new ArrayList<Integer>();
+            for(IntWritable value:values) {
+                System.out.println(value.get());//디버
+                if (value.get() == -1) isTop5=1;
+                else arr.add(value.get());
+            }
+            if (isTop5==0) return;
+            System.out.println("key!");
+            System.out.println(key.toString());
+            System.out.println(arr.size());
+
+            for(int value:arr) {
+                System.out.println(value);
+                //디버그 목적
+                context.write(new IntWritable(value),new IntWritable(1));
+                //locCount[value]=locCount[value]+1;
+            }
+            return;
+        }
+    }
+
+
+/*
+
+    public int run(String[] args) throws Exception {
+
+        JobControl jobControl = new JobControl("jobChain");
+        Configuration conf1 = getConf();
+
+        //job1
+        Job job1 = Job.getInstance(conf1);
+        job1.setJarByClass(WordCombined2.class);
+        job1.setJobName("Word Combined");
+
+
+        //job1.setMapperClass(WordCombined2.R2mapper1.class);
+        job1.setReducerClass(WordCombined2.R2SumReducer.class);
+        job1.setCombinerClass(WordCombined2.R2SumReducer.class);
+
+        job1.setOutputKeyClass(IntWritable.class);
+        job1.setOutputValueClass(IntWritable.class);
+
+        MultipleInputs.addInputPath(job1, new Path(args[1] + "/R1"), TextInputFormat.class, R2mapper1.class);
+        MultipleInputs.addInputPath(job1, new Path(args[0]),TextInputFormat.class, R2mapper1.class);
+        //FileInputFormat.setInputPaths(job1, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job1, new Path(args[1] + "/temp2"));
+
+        ControlledJob controlledJob1 = new ControlledJob(conf1);
+        controlledJob1.setJob(job1);
+
+
+        //job2고고! R1정렬해주는거!
+        Configuration conf2 = getConf();
+
+        Job job2 = Job.getInstance(conf2);
+        job2.setJarByClass(WordCombined2.class);
+        job2.setJobName("Word Invert");
+
+        FileInputFormat.setInputPaths(job2, new Path(args[1] + "/temp2"));
+        FileOutputFormat.setOutputPath(job2, new Path(args[1] + "/R2-final"));
+
+        job2.setMapperClass(WordCombined2.R2WordMapper2.class);
+        job2.setReducerClass(WordCombined2.R2SumReducer2.class);
+        job2.setCombinerClass(WordCombined2.R2SumReducer2.class);
+
+        job2.setOutputKeyClass(IntWritable.class);
+        job2.setOutputValueClass(IntWritable.class);
+        job2.setInputFormatClass(KeyValueTextInputFormat.class);
+
+        job2.setSortComparatorClass(WordCombined2.IntComparator.class);
+        ControlledJob controlledJob2 = new ControlledJob(conf2);
+        controlledJob2.setJob(job2);
+
+        // make job2 dependent on job1
+        controlledJob2.addDependingJob(controlledJob1);
+
+
+        // add the job to the job control
+        jobControl.addJob(controlledJob1);
+        jobControl.addJob(controlledJob2);
+
+        Thread jobControlThread = new Thread(jobControl);
+        jobControlThread.start();
+
+        while (!jobControl.allFinished()) {
+            System.out.println("Jobs in waiting state: " + jobControl.getWaitingJobList().size());
+            System.out.println("Jobs in ready state: " + jobControl.getReadyJobsList().size());
+            System.out.println("Jobs in running state: " + jobControl.getRunningJobList().size());
+            System.out.println("Jobs in success state: " + jobControl.getSuccessfulJobList().size());
+            System.out.println("Jobs in failed state: " + jobControl.getFailedJobList().size());
+            try {
+                Thread.sleep(5000);
+            } catch (Exception e) {
+
+            }
+
+        }
+        System.exit(0);
+        return (job1.waitForCompletion(true) ? 0 : 1);
+    }
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new WordCombined2(), args);
+        System.exit(exitCode);
+    }
+*/
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration conf = new Configuration();
+        Job job = new Job(conf, "Reduce-side join");
+        job.setJarByClass(WordCombined2.class);
+        job.setReducerClass(R2SumReducer.class);
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        MultipleInputs.addInputPath(job, new Path(args[1] + "/R1"),TextInputFormat.class, R2mapper1.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]),TextInputFormat.class, R2mapper1.class);
+        Path outputPath = new Path(args[1] + "/R2");
+
+        FileOutputFormat.setOutputPath(job, outputPath);
+        outputPath.getFileSystem(conf).delete(outputPath);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
+
+
